a0c9c91af8cc223b91c0255ca6f7b6d33f589af2
davmarksman <david.ike.mark@gmail.com>
2021-04-25 16:59:18 +0100

    NavPad: Test rotary encoder as arrow keys.

--- keyboards/handwired/_davNav/keymaps/default/keymap.c
+++ keyboards/handwired/_davNav/keymaps/default/keymap.c
@@ -27,12 +27,13 @@ enum layer_names {
 #define LSYS_HOME LT(SYS,KC_HOME)
 #define KT_C_END LCTL_T(KC_END)
 #define LSYS LT(SYS,KC_NO)
+#define K_UNDO LCTL(KC_Z)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
-    KC_HOME  ,KC_UP    ,KT_C_END   , 
-    KC_LEFT    ,KC_DOWN  ,KC_RGHT   ,LSYS
+    LSYS_HOME  ,KC_UP    ,KT_C_END   , 
+    KC_LEFT    ,KC_DOWN  ,KC_RGHT   ,K_UNDO
   ),
   [SYS] = LAYOUT(
     KC_NO     ,KC_NO     ,RESET     ,
@@ -42,12 +43,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // keyboards\handwired\6davkey\keymaps\default\keymap.c
 
-// qmk compile -kb handwired/6davkey -km default
+// qmk compile -kb handwired/_davNav -km default
 
 void encoder_update_user(uint8_t index, bool clockwise) {
+  // if (clockwise) {
+  //   tap_code16(C(KC_Y));
+  // } else {
+  //   tap_code16(C(KC_Z));
+  // }
   if (clockwise) {
-    tap_code16(C(KC_Y));
+    tap_code16(KC_RGHT);
   } else {
-    tap_code16(C(KC_Z));
+    tap_code16(KC_LEFT);
   }
 }
+
