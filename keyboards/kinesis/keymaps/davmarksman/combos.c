d5c494f9bd968da778158fe1bcb6e74a867e4b65
davmarksman <david.ike.mark@gmail.com>
2021-05-24 23:43:25 +0100

    copy, paste, all, find combos

--- keyboards/kinesis/keymaps/davmarksman/combos.c
+++ keyboards/kinesis/keymaps/davmarksman/combos.c
@@ -12,14 +12,21 @@ enum combos {
   KU_Q,
   FO_FOR,
   TD_THE,
-  TH_TH,
+  TF_TH,
   TN_ENT,
   IN_ING,
   ON_ION,
   DN_AND, 
   OR_FOR, 
   DM_DAV, 
-  BK_BROK, 
+  BR_BROK, 
+  BO_BROK,
+
+  // Shortcuts
+  BM_COPY,//xc qwerty
+  BD_PASTE, //cv qwerty
+  CL_ALL, // we qwerty
+  LF_FIND, // er qwerty
 
   // This must be the last item in the enum.
   // This is used to automatically update the combo count.
@@ -32,14 +39,20 @@ uint16_t COMBO_LEN = COMBO_LENGTH;
 const uint16_t PROGMEM ku_combo[] = {KC_K, KC_U, COMBO_END};
 const uint16_t PROGMEM fo_combo[] = {KC_F, KC_O, COMBO_END};
 const uint16_t PROGMEM td_combo[] = {KC_T, KC_D, COMBO_END};
-const uint16_t PROGMEM th_combo[] = {KC_T, KC_H, COMBO_END};
+const uint16_t PROGMEM tf_combo[] = {KC_T, KC_F, COMBO_END};
 const uint16_t PROGMEM tn_combo[] = {KC_T, LSYN_N, COMBO_END};
 const uint16_t PROGMEM in_combo[] = {KC_I, LSYN_N, COMBO_END};
 const uint16_t PROGMEM on_combo[] = {KC_O, LSYN_N, COMBO_END};
 const uint16_t PROGMEM dn_combo[] = {KC_D, LSYN_N, COMBO_END};
 const uint16_t PROGMEM or_combo[] = {KC_O, KC_R, COMBO_END};
-const uint16_t PROGMEM dm_combo[] = {KC_D, KC_M, COMBO_END};
-const uint16_t PROGMEM bk_combo[] = {KC_B, KC_K, COMBO_END};
+const uint16_t PROGMEM dv_combo[] = {KC_D, KC_V, COMBO_END};
+const uint16_t PROGMEM br_combo[] = {KC_B, HOME_C_R, COMBO_END};
+const uint16_t PROGMEM bo_combo[] = {KC_B, KC_O, COMBO_END};
+
+const uint16_t PROGMEM bm_combo[] = {KC_B, KC_M, COMBO_END};
+const uint16_t PROGMEM bd_combo[] = {KC_B, KC_D, COMBO_END};
+const uint16_t PROGMEM cl_combo[] = {KC_C, KC_L, COMBO_END};
+const uint16_t PROGMEM lf_combo[] = {KC_F, KC_L, COMBO_END};
 
 
 
@@ -47,14 +60,19 @@ combo_t key_combos[] = {
   [KU_Q] = COMBO_ACTION(ku_combo),
   [FO_FOR] = COMBO_ACTION(fo_combo),
   [TD_THE] = COMBO_ACTION(td_combo),
-  [TH_TH] = COMBO_ACTION(th_combo),
+  [TF_TH] = COMBO_ACTION(tf_combo),
   [TN_ENT] = COMBO_ACTION(tn_combo),
   [IN_ING] = COMBO_ACTION(in_combo),
   [ON_ION] = COMBO_ACTION(on_combo),
   [DN_AND] = COMBO_ACTION(dn_combo),
-  [OR_FOR] = COMBO_ACTION(or_combo),
-  [DM_DAV] = COMBO_ACTION(dm_combo),
-  [BK_BROK] = COMBO_ACTION(bk_combo),
+  [OR_FOR] = COMBO_ACTION(or_combo), // maybe remove
+  [DM_DAV] = COMBO_ACTION(dv_combo),
+  [BR_BROK] = COMBO_ACTION(br_combo),
+  [BO_BROK] = COMBO_ACTION(bo_combo),
+  [BM_COPY] = COMBO_ACTION(bm_combo),
+  [BD_PASTE] = COMBO_ACTION(bd_combo),
+  [CL_ALL] = COMBO_ACTION(cl_combo),
+  [LF_FIND] = COMBO_ACTION(lf_combo),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
@@ -90,7 +108,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         }
       }
       break;
-    case TH_TH:
+    case TF_TH:
       if (pressed) {
         if (mod_state & MOD_MASK_SHIFT) {
             del_mods(MOD_MASK_SHIFT);
@@ -153,7 +171,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
           }
         }
         break;
-    case BK_BROK:
+    case BR_BROK:
       if (pressed) {
           if (mod_state & MOD_MASK_SHIFT) {
               del_mods(MOD_MASK_SHIFT);
@@ -165,5 +183,45 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
           }
         }
         break;
+    case BO_BROK:
+      if (pressed) {
+          if (mod_state & MOD_MASK_SHIFT) {
+              del_mods(MOD_MASK_SHIFT);
+              send_string("Broker");
+              set_mods(mod_state);
+          }
+          else {
+              send_string("broker");
+          }
+        }
+        break;
+    case BM_COPY:
+      if (pressed) {
+        register_code(KC_LCTRL);
+        tap_code(KC_C);
+        unregister_code(KC_LCTRL);
+      }
+      break;
+    case BD_PASTE:
+      if (pressed) {
+        register_code(KC_LCTRL);
+        tap_code(KC_V);
+        unregister_code(KC_LCTRL);
+      }
+      break;
+    case CL_ALL:
+      if (pressed) {
+        register_code(KC_LCTRL);
+        tap_code(KC_A);
+        unregister_code(KC_LCTRL);
+      }
+      break;
+    case LF_FIND:
+      if (pressed) {
+        register_code(KC_LCTRL);
+        tap_code(KC_F);
+        unregister_code(KC_LCTRL);
+      }
+      break;
   }
 }
