85f85a8e9ed0c49f873183641f0f007dcca43c89
davmarksman <david.ike.mark@gmail.com>
2021-05-26 14:38:11 +0100

    part of last commit

--- keyboards/keycapsss/kimiko/keymaps/davmarksman/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/davmarksman/keymap.c
@@ -35,26 +35,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                 `----------------------------------'       '------------------------------------'
  */
 
+//  TODO: replace a tab with Del
  [_BASE] = LAYOUT(
     KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_NUBS,
     KC_TAB,   KC_Z,   KC_C,    KC_L,    KC_F,    KC_EQL,                      TD(TD_MINS),KC_K,    KC_U,    KC_H,    KC_J,    KC_BSLS,
-    K_ALT_TAB,KC_W,   KC_S,    HOME_C_R,KC_T,    KC_G,                        KC_Y,       KC_I,    KC_E,    KC_O,    KC_A,  KC_QUOT,
-    K_CTLSFT, KC_X,   KC_B,    KC_M,    KC_D,    KC_V,    K_CLIP,  K_AHK,     TD(TD_SCLN),KC_P,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV,
-                      KC_LEFT, KC_RGHT, KC_TAB,  K_OSFT,  KT_C_BK, L1_SPC,    LSYN_N,     KC_ENT,  KC_LALT, KC_RGUI
+    KC_Q,     KC_W,   KC_S,    HOME_C_R,KC_T,    KC_G,                        KC_Y,       KC_I,    KC_E,    KC_O,    KC_A,    KC_QUOT,
+    KC_DEL,   KC_X,   KC_B,    KC_M,    KC_D,    KC_V,    K_CLIP,  K_AHK,     TD(TD_SCLN),KC_P,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV,
+                      KC_LEFT, KC_RGHT, KT_A_TAB,K_OSFT,  KT_C_BK, L1_SPC,    LSYN_N,     KC_ENT,  KC_RALT, KC_RGUI
 ),
 
 [_GAME] = LAYOUT(
     KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_NUBS,
     KC_EQL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    LBASE,  LQ,         KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_LEFT, KC_RGHT, XXXXXXX, KC_LSFT, KT_C_BK, KC_ENT,    KC_SPC,  XXXXXXX, KC_LALT, KC_RGUI
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    LBASE,  LGAME,      KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                      KC_LEFT, KC_RGHT, XXXXXXX, K_OSFT, KT_C_BK, KC_SPC,    XXXXXXX,  KC_ENT,   KC_LALT, KC_RGUI
 ),
 [_L1] = LAYOUT(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, K_UNDOTB,                    XXXXXXX, XXXXXXX, A(KC_LEFT),A(KC_RGHT),KC_NO,KC_F12,
     XXXXXXX, XXXXXXX, KC_LBRC, KC_LCBR, KC_LPRN, K_GLOBAL,                    XXXXXXX, KC_AT,   KC_UNDS, KC_ASTR, XXXXXXX, XXXXXXX,
-    KC_CAPS, KS_X2X,  KC_RBRC, KC_RCBR, KC_RPRN, XXXXXXX,   LBASE,  LQ,       K_SNIP,  K_SNIP,  XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,
+    KC_CAPS, KS_X2X,  KC_RBRC, KC_RCBR, KC_RPRN, XXXXXXX,   LBASE,  LGAME,    K_SNIP,  K_SNIP,  XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,
                       KC_HOME, KC_END,  XXXXXXX, KC_PPLS,   KC_DEL, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX
 ),
 
@@ -62,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     XXXXXXX, XXXXXXX, K_GRV3,  K_AND,   K_OR,    XXXXXXX,                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_F12,
     XXXXXXX, K_EQ_GR, KC_EXLM, KC_AMPR, K_PIPE,  KC_DLR,                      KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-    _______, KS_X2X,  K_AT,    XXXXXXX, XXXXXXX, XXXXXXX,  LBASE, LQ,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    _______, KS_X2X,  K_AT,    XXXXXXX, XXXXXXX, XXXXXXX,  LBASE, LGAME,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                       XXXXXXX, XXXXXXX, RCS(KC_NO),KC_LSFT, KT_C_DEL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
