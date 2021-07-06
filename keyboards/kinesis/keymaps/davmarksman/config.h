6e818a73d1899acdd1e23814b7c855aa73d161ab
davmarksman <david.ike.mark@gmail.com>
2021-07-06 22:30:03 +0100

    reduce timings for combo and tapping

--- keyboards/kinesis/keymaps/davmarksman/config.h
+++ keyboards/kinesis/keymaps/davmarksman/config.h
@@ -4,10 +4,12 @@
 
 // Home row mods settings //
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 210
-#define COMBO_TERM 70
+#define TAPPING_TERM 200
+#define COMBO_TERM 40
 
 // Limits the max. amount of layers to 8 to save firmware memory.
 #define LAYER_STATE_8BIT
 
 #define ONESHOT_TIMEOUT 1000  /* Time (in ms) before the one shot key is released */
+
+
