8ff44c4934d85da7371255ef21d849736ffd0a6f
davmarksman <david.ike.mark@gmail.com>
2023-01-08 11:58:04 +0000

    Add zom in and out, fix lighting

--- keyboards/dmqdesign/spin/keymaps/davmarksman/keymap.c
+++ keyboards/dmqdesign/spin/keymaps/davmarksman/keymap.c
@@ -4,16 +4,18 @@
 
 enum layers {
     _APPS,
-    _FNPAD,
-    _NAV,
-    _RGB,
+    // _RGB,
     _LYRICS,
+    _CODE
 };
 // Window organisation layer (or put on fn pad)
 
 enum custom_keycodes {
     DAVMARKSMAN = SAFE_RANGE,
     K_WINMOVE,
+    K_TEST_R,
+    K_TEST_D,
+    K_TEST_L
 };
 
 // Apps
@@ -34,54 +36,53 @@ enum custom_keycodes {
 
 #define LFN TO(_FNPAD)
 #define LAPP TO(_APPS)
-#define LNAV TO(_NAV)
-#define LRGB TO(_RGB)
+// #define LRGB TO(_RGB)
 #define LLYR TO(_LYRICS)
+#define LCODE OSL(_CODE)
 
 // Window layout
 #define K_RDESK G(KC_RGHT)
 #define K_LDESK G(KC_LEFT)
 
+// Useful defines
+#define K_GLOBAL C(S(KC_F))
+#define K_ZOOM_RST C(KC_KP_0)
+#define K_ZOOM_RST2 C(KC_0)
+// #define K_ZOOM_IN LCTL(KC_WH_D)
+// #define K_ZOOM_OUT LCTL(KC_WH_U)
+#define K_ZOOM_IN LCTL(KC_PPLS)
+#define K_ZOOM_OUT LCTL(KC_PMNS)
+// KC_KP_0
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_APPS] = LAYOUT(/* Base */
-                KA_FIREFX,  KA_CHROME,  KA_NOTE,    LLYR,
-                KA_VSCODE,  KA_VS19,    KA_SQL,     LFN,
-                KA_EXPLR,   K_LDESK,    K_RDESK,    LNAV,
+                KA_FIREFX,  KA_CHROME,  KA_NOTE,    LCODE,
+                KA_VSCODE,  KA_VS19,    KA_SQL,     K_ZOOM_RST,
+                KA_EXPLR,   K_LDESK,    K_RDESK,    LLYR,
                 K_SAVE,     K_WINMOVE,  KA_SPOT
                 ),
-    [_FNPAD] = LAYOUT(
-                KC_F1,      KC_F2,      KC_F3,      LAPP,
-                KC_F4,      KC_F5,      KC_F6,      LFN,
-                KC_F7,      KC_F8,      KC_F9,      LNAV,
-                KC_F10,     KC_F11,     KC_F12
-                ),
-    [_NAV] = LAYOUT(
-                K_WINMOVE,  XXXXXXX,    KC_PGUP,    LAPP,
-                K_LDESK,    K_RDESK,    KC_PGDN,    LFN,
-                KC_HOME,    KC_UP,      KC_END,     LNAV,
-                KC_LEFT,    KC_DOWN,    KC_RGHT
-                ),
-    [_RGB] = LAYOUT(
-                RGB_HUI,  RGB_SAI, RGB_VAI, LAPP,
-                RGB_HUD,  RGB_SAD, RGB_VAD, LFN,
-                RGB_M_P,  XXXXXXX, XXXXXXX, LNAV,
-                RGB_RMOD, RGB_TOG, RGB_MOD
-                ),
+    // [_RGB] = LAYOUT(
+    //             RGB_HUI,  RGB_SAI, RGB_VAI, LAPP,
+    //             RGB_HUD,  RGB_SAD, RGB_VAD, LFN,
+    //             RGB_M_P,  XXXXXXX, XXXXXXX, LNAV,
+    //             RGB_RMOD, RGB_TOG, RGB_MOD
+    //             ),=
     [_LYRICS] = LAYOUT(
-                KC_UP,  XXXXXXX, C(KC_INS), LAPP,
-                KC_DOWN,  XXXXXXX, XXXXXXX, LFN,
-                C(A(KC_LEFT)), C(KC_ENT), C(A(KC_RGHT)), LNAV,
-                A(KC_LEFT),  C(KC_SPC), XXXXXXX
+                KC_UP,         XXXXXXX,   C(KC_INS),     LAPP,
+                KC_DOWN,       XXXXXXX,   XXXXXXX,       ,
+                C(A(KC_LEFT)), C(KC_ENT), C(A(KC_RGHT)), LLYR,
+                A(KC_LEFT),    C(KC_SPC), XXXXXXX
+                ),
+    [_CODE] = LAYOUT(
+                K_TEST_L,  K_TEST_R,  K_TEST_D,      LAPP,
+                C(KC_A),  XXXXXXX,    XXXXXXX,       ,
+                A(KC_E),  K_GLOBAL,  XXXXXXX,        LLYR,
+                XXXXXXX,  XXXXXXX,  XXXXXXX      
                 )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case DAVMARKSMAN:
-            if (record->event.pressed) {
-                SEND_STRING("davmarksman");
-            }
-            break;
         case K_WINMOVE:
             if (record->event.pressed) {
                 tap_code16(G(S(KC_LEFT)));
@@ -89,6 +90,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code16(G(KC_UP));
             }
             break;  
+        case K_TEST_R:
+            if (record->event.pressed) {
+                tap_code16(C(KC_R));
+                _delay_ms(100);
+                tap_code16(KC_T);
+            }
+            break;
+        case K_TEST_D:
+            if (record->event.pressed) {
+                tap_code16(C(KC_R));
+                _delay_ms(100);
+                tap_code16(C(KC_T));
+            }
+            break;
+        case K_TEST_L:
+            if (record->event.pressed) {
+                tap_code16(C(KC_R));
+                _delay_ms(100);
+                tap_code16(KC_L);
+            }
+            break;
     }
 
     return true;
@@ -96,31 +118,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder */
-        // switch (get_highest_layer(layer_state)) {     //break each encoder update into a switch statement for the current layer           
-        //     // change layers
-        //     default:
-        //         if (clockwise) {
-        //             layer_move(_RGB);
-        //         } else {
-        //             layer_move(_LYRICS);
-        //         }
-        //         break;
-        //     // 
-        //     case _RGB:
-        //         if (clockwise) {
-        //             layer_move(_LYRICS);
-        //         } else {
-        //             layer_move(_APPS);
-        //         }
-        //         break;
-        //     case _LYRICS:
-        //         if (clockwise) {
-        //             layer_move(_APPS);
-        //         } else {
-        //             layer_move(_RGB);
-        //         }
-        //         break;
-        // }
+        // Rotate tabs
         switch (get_highest_layer(layer_state)) {     //break each encoder update into a switch statement for the current layer           
             case _APPS:
                 if (clockwise) {
@@ -137,53 +135,28 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 }
                 break;
         }
-
     } else if (index == 1) { /* Second encoder */
+        // Zoom in out
+        // register_code(KC_LCTL);
+        // _delay_ms(60);
+        // if (clockwise) {
+        //    tap_code(KC_WH_D);
+        // } else {
+        //     tap_code16(KC_WH_U);
+        // }  
+        // _delay_ms(60);
+        // unregister_code(KC_LCTL);
         if (clockwise) {
-           tap_code16(C(KC_Y));
+           tap_code16(LCTL(KC_PPLS));
+        } else {
+            tap_code16(LCTL(KC_PMNS));
+        } 
+    } else if (index == 2) { /* Third encoder */
+        // Undo redo
+        if (clockwise) {
+            tap_code16(C(KC_Y));
         } else {
             tap_code16(C(KC_Z));
-        }
-        // switch (get_highest_layer(layer_state)) {
-        //     case _NAV:
-        //         if (clockwise) {
-        //             tap_code(KC_PGDN);
-        //         } else {
-        //             tap_code(KC_PGUP);
-        //         }
-        //         break;
-        //     case _RGB:
-        //         if (clockwise) {
-        //             rgblight_increase_sat();
-        //         } else {
-        //             rgblight_decrease_sat();
-        //         }
-        //         break;
-        //     default:
-        //         // Scroll tabs
-        //         if (clockwise) {
-        //             tap_code16(C(KC_TAB));
-        //         } else {
-        //             tap_code16(S(C(KC_TAB)));
-        //         }
-        //         break;
-        // }
-    } else if (index == 2) { /* Third encoder */
-        switch (get_highest_layer(layer_state)) {
-            case _RGB:
-                if (clockwise) {
-                    rgblight_increase_val();
-                } else {
-                    rgblight_decrease_val();
-                }
-                break;
-            default:
-                if (clockwise) {
-                    tap_code16(C(KC_Y));
-                } else {
-                    tap_code16(C(KC_Z));
-                }
-                break;
         }
     }
     return true;
@@ -194,22 +167,20 @@ layer_state_t layer_state_set_user(layer_state_t state) { //This will run every 
         case _APPS:
             rgblight_mode(RGBLIGHT_MODE_BREATHING);
             break;
-        case _FNPAD:
-            rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL);
-            break;
-        case _NAV:
-            rgblight_mode(RGBLIGHT_MODE_KNIGHT);
-            break;
-        case _RGB:
-            rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
-            setrgb(RGB_RED, &led[0]);
-            setrgb(RGB_SPRINGGREEN, &led[1]);
-            setrgb(RGB_BLUE, &led[2]);
-            // setrgb(0, 0, 0, &led[2]); - no colour
+        // case _FNPAD:
+        //     rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL);
+        //     break;
+        // case _GAME:
+        //     rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+        //     rgblight_setrgb_at(RGB_SPRINGGREEN, 0);
+        //     rgblight_setrgb_at(RGB_YELLOW, 1);
+        //     rgblight_setrgb_at(RGB_BLUE, 2);
+        //     break;
+        case _CODE:
+            rgblight_setrgb(RGB_GOLD);
             break;
         case _LYRICS:
-            rgblight_mode(RGBLIGHT_MODE_SNAKE);
-            // setrgb(0, 0, 0, &led[2]); - no colour
+            rgblight_setrgb(RGB_RED);
             break;
     }
     rgblight_set();
@@ -281,17 +252,8 @@ void oled_task_user(void) {
     oled_write_P(PSTR("Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
-        case _FNPAD:
-            oled_write_P(PSTR("FUNCTIONS\n"), false);
-            break;
-        case _RGB:
-            oled_write_P(PSTR("RGB\n"), false);
-            break;
         case _APPS:
             oled_write_P(PSTR("APPS - PROGRAM\n"), false);
-            break;
-        case _NAV:
-            oled_write_P(PSTR("NAVIGATION\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
